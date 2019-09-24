78eba4fd1e0f0dbb53a2f9128c9e78e677e2c044
Andreas Müller <taori@users.noreply.github.com>
2019-09-24 19:27:25 +0200

    Debugging + Runtime Compilation for standalone new project

--- src/UntouchedRecompileTest/Startup.cs
+++ src/UntouchedRecompileTest/Startup.cs
@@ -23,7 +23,9 @@ namespace UntouchedRecompileTest
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
-			services.AddControllersWithViews();
+			services
+				.AddControllersWithViews()
+				.AddRazorRuntimeCompilation();
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,7 +41,7 @@ namespace UntouchedRecompileTest
 				// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
 				app.UseHsts();
 			}
-//			app.UseHttpsRedirection();
+			//			app.UseHttpsRedirection();
 			app.UseStaticFiles();
 
 			app.UseRouting();
