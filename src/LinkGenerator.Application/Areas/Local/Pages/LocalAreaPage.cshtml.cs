07abc5622138f1e4b11dace34315058a238b857c
Andreas Müller <taori@users.noreply.github.com>
2019-10-13 04:46:32 +0200

    fixed link generation

---
+++ src/LinkGenerator.Application/Areas/Local/Pages/LocalAreaPage.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace LinkGenerator.Application.Areas.Local.Pages
+{
+    public class LocalAreaPageModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
