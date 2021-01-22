d33e787e00afb18fea10b767e8c19736205af9dd
Andreas Müller <taori@users.noreply.github.com>
2021-01-22 16:01:33 +0100

    win32 access project

---
+++ src/win32access/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace win32access
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			// todo add Microsoft.Windows.CsWin32
+			// see https://www.heise.de/news/Microsoft-hilft-beim-Zugriff-auf-die-Win32-API-aus-C-Rust-und-Co-5032816.html
+		}
+	}
+}
